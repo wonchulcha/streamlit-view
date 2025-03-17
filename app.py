60b592f213fbbc9013e9ac2fc65abc632a9471f3
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:25:28 +0900

    numpy exam-01

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 
-st.write("테이블 생성을 위한 데이터 사용 첫번째 시도")
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+
+df = pd.DataFrame(
+  np.random.randn(10, 20),
+  columns=('col %d' % i for i in range(20))
+)
 
 st.write(df)
 
