c68cf80ff04b1574ea40354b30fd38d9fbb034a8
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:28:44 +0900

    numpy exam-04

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import numpy as np
 
 
 df = pd.DataFrame(
-  np.random.randn(20, 3),
-  columns=['a', 'b', 'c']
+  np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+  columns=['lat', 'lon']
 )
 
-st.line_chart(df)
+st.map(df)
 
 
 
