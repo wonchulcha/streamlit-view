779287af160005a6d07d3f03a282e5c8b0a98bbe
wonchulcha <wonchulcha@gmail.com>
2025-03-17 17:01:11 +0900

    database conn

--- app.py
+++ app.py
@@ -3,19 +3,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-import time
+conn = st.connection('mysql', type='sql')
+df = conn.query('SELECT * FROM test')
 
-'Starting a long computation...'
-
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i + 1)
-    time.sleep(0.1)
-
-'...and now we\'re done!'
+st.dataframe(df)
 
 
 
