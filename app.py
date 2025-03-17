f8da919b9260d40091adf2716b842809ddd85b9c
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:01:17 +0900

    change text in korean

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 view = [100, 150, 30]
-st.write('# Youtube View')
-st.write('## raw')
+st.write('# Streamlit 연습')
+st.write('## 간단한 배열 뷰')
 view
-st.write('## bar chart')
+st.write('## 바 차트')
 st.bar_chart(view)
 
 sview = pd.Series(view)
