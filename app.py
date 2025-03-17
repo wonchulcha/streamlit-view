2b99a073f6a05fa1a53345a4551bb6e9f712ef66
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:11:28 +0900

    pandas exam-02

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+
+st.write("테이블 생성을 위한 데이터 사용 첫번째 시도")
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
-df
+st.write(df)
 
 
 
