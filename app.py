5e9316a480f8b71bf9e862f2068cd1bbe3679a18
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:49:29 +0900

    time

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 
+import time
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
+'Starting a long computation...'
 
-option = st.selectbox(
-    'Which number do you like best?',
-    df['first column']
-)
+latest_iteration = st.empty()
+bar = st.progress(0)
 
-'You selected: ', option
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'...and now we\'re done!'
+
 
 
 
