4594d15482a825b8d73d2c7ef14d4fc19c6c141c
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:46:42 +0900

    selectbox

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import pandas as pd
 import numpy as np
 
 
-if st.checkbox('Show/Hide'):
-    chart_data = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns=['a', 'b', 'c']
-    )
-    st.line_chart(chart_data)
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+option = st.selectbox(
+    'Which number do you like best?',
+    df['first column']
+)
+
+'You selected: ', option
 
 
 
