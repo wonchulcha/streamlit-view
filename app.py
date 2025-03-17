b26d3e8e58e7b04f3edd5a6d4e8f28e340c6c93e
wonchulcha <wonchulcha@gmail.com>
2025-03-17 16:08:59 +0900

    pandas exam-01

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-view = [100, 150, 30]
-st.write('# Streamlit 연습')
-st.write('## 간단한 배열 뷰')
-view
-st.write('## 바 차트')
-st.bar_chart(view)
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
 
-sview = pd.Series(view)
-sview
+df
+
+
+
