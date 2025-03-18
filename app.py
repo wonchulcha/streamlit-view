ea7cf2940f787837ef26c479336baaef93685cbe
wonchulcha <wonchulcha@gmail.com>
2025-03-18 09:36:10 +0900

    st. command

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-conn = st.connection('mysql')
-df = conn.query('SELECT * FROM buytbl')
+st.button("Click me")
+st.download_button("Download file", data)
+st.link_button("Go to gallery", url)
+st.page_link("app.py", label="Home")
+st.data_editor("Edit data", data)
+st.checkbox("I agree")
+st.feedback("thumbs")
+st.pills("Tags", ["Sports", "Politics"])
+st.radio("Pick one", ["cats", "dogs"])
+st.segmented_control("Filter", ["Open", "Closed"])
+st.toggle("Enable")
+st.selectbox("Pick one", ["cats", "dogs"])
+st.multiselect("Buy", ["milk", "apples", "potatoes"])
+st.slider("Pick a number", 0, 100)
+st.select_slider("Pick a size", ["S", "M", "L"])
+st.text_input("First name")
+st.number_input("Pick a number", 0, 10)
+st.text_area("Text to translate")
+st.date_input("Your birthday")
+st.time_input("Meeting time")
+st.file_uploader("Upload a CSV")
+st.audio_input("Record a voice message")
+st.camera_input("Take a picture")
+st.color_picker("Pick a color")
 
-st.dataframe(df)
+# # Use widgets' returned values in variables:
+# for i in range(int(st.number_input("Num:"))):
+#     foo()
+# if st.sidebar.selectbox("I:",["f"]) == "f":
+#     b()
+# my_slider_val = st.slider("Quinn Mallory", 1, 88)
+# st.write(slider_val)
 
+# # Disable widgets to remove interactivity:
+# st.slider("Pick a number", 0, 100, disabled=True)
 
 
 
