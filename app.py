28a3497882c27880885ab8864700667e765502b6
wonchulcha <wonchulcha@gmail.com>
2025-03-18 11:30:58 +0900

    3rd

--- app.py
+++ app.py
@@ -14,18 +14,7 @@ st.radio("Pick one", ["cats", "dogs"])
 st.segmented_control("Filter", ["Open", "Closed"])
 st.toggle("Enable")
 st.selectbox("Pick one", ["cats", "dogs"])
-st.multiselect("Buy", ["milk", "apples", "potatoes"])
-st.slider("Pick a number", 0, 100)
-st.select_slider("Pick a size", ["S", "M", "L"])
-st.text_input("First name")
-st.number_input("Pick a number", 0, 10)
-st.text_area("Text to translate")
-st.date_input("Your birthday")
-st.time_input("Meeting time")
-st.file_uploader("Upload a CSV")
-st.audio_input("Record a voice message")
-st.camera_input("Take a picture")
-st.color_picker("Pick a color")
+
 
 # # Use widgets' returned values in variables:
 # for i in range(int(st.number_input("Num:"))):
